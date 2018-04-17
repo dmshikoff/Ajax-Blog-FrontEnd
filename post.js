87d51e3b97d874f6aed9ead55b69fc9e2eee6392
dmshikoff <dmshikoff@gmail.com>
2018-04-17 13:54:50 -0700

    working on front end javascript code

---
+++ post.js
@@ -0,0 +1,26 @@
+const newPost = document.querySelector('#create-post')
+
+newPost.addEventListener('click', event => {
+    const formContainer = document.querySelector('#view')
+    formContainer.classList.remove('d-none')
+})
+
+function createDatButt(post){
+    const newButt = document.createElement('button')
+    newButt.classList.add('list-group-item', 'list-group-item-action')
+    newButt.id = post.id
+    newButt.innerHTML = post.title
+    newButt.addEventListener('click', renderPost)
+    return newButt
+}
+
+const sidebar = document.querySelector('#sidebar')
+axios.get('http://localhost:3000/posts')
+    .then( response => {
+        const titles = []
+        const posts = response.data.data
+        posts.forEach( ele => {
+            console.log(createDatButt(ele))
+            // sidebar.appendChild(createDatButt(ele))
+        })
+    })
